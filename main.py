78eae533fe6d836c9dfe490f300a3955098299f4
Shivanjan Chakravorty <schakravorty846@gmail.com>
2021-04-27 18:03:08 +0530

    remove exclusive

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import pika, sys, os, json
-# from tools.c7n_org.c7n_org.override import overrun
-from c7n_org.override import overrun
+from tools.c7n_org.c7n_org.override import overrun
+# from c7n_org.override import overrun
 # from async_publisher import CustodianPublisher
 import time
 from dotenv import load_dotenv
@@ -96,11 +96,11 @@ def main():
     channel = connection.channel()
 
     channel.basic_consume(queue=os.getenv("RABBITMQ_AWS_QUEUE"), on_message_callback=aws_callback,
-                          auto_ack=True, exclusive=True)
+                          auto_ack=True)
     channel.basic_consume(queue=os.getenv("RABBITMQ_GCP_QUEUE"), on_message_callback=gcp_callback,
-                          auto_ack=True, exclusive=True)
+                          auto_ack=True)
     channel.basic_consume(queue=os.getenv("RABBITMQ_AZURE_QUEUE"), on_message_callback=azure_callback,
-                          auto_ack=True, exclusive=True)
+                          auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
